d195322d7c1d1601a1bdf1d2bdb02eae38bdd0ab
SEEMG <SEEMG@00EE000000DT00.mshome.net>
2024-11-27 16:37:54 -0300

    Segundo commit

--- primeiroPrograma/src/primeiroPrograma/Executavel.java
+++ primeiroPrograma/src/primeiroPrograma/Executavel.java
@@ -3,8 +3,23 @@ package primeiroPrograma;
 public class Executavel {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Olá, Mundo!");
+		// Criei variáveis inteiro, decimal e Texto.
+		int inteiro;
+		double numeroDecimal;
+		String texto; 
+		
+		//Atribuição de valores as variáveis.
+		inteiro = 5;
+		numeroDecimal = 5.65;
+		texto = "Tales";
+		
+		System.out.println("Nome: " + texto);
+		System.out.println("Numero inteiro: " + inteiro);
+		System.out.println("Numero decimal: " + numeroDecimal);
+		
+		
+		
 	}
 
 }
